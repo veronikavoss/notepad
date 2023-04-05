f599ad6cd712a1ed94648abfb242a48828dabbd2
veronikavoss <veronikavoss.none@gmail.com>
2023-04-05 14:45:22 +0900

    first commit

---
+++ notepad.py
@@ -0,0 +1,97 @@
+import sys
+from PySide6.QtWidgets import (
+    QApplication,QMainWindow,QWidget,QGridLayout,QVBoxLayout,
+    QPlainTextEdit,QMenuBar,QMenu,QStatusBar)
+
+from PySide6.QtGui import QAction
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.set_ui()
+        self.show()
+    
+    def set_ui(self):
+        self.setWindowTitle('notepad')
+        self.resize(1280,720)
+        
+        self.set_menubar()
+        
+        self.central_widget = QWidget(self)
+        
+        # self.grid_layout = QGridLayout(self.central_widget)
+        # self.grid_layout.setContentsMargins(0,0,0,0)
+        
+        self.vlayout = QVBoxLayout(self.central_widget)
+        self.vlayout.setContentsMargins(0,0,0,0)
+        
+        self.plain_text_edit = QPlainTextEdit(self.central_widget)
+        self.vlayout.addWidget(self.plain_text_edit)
+        
+        # self.grid_layout.addLayout(self.vlayout,0,0,1,1)
+        
+        self.setCentralWidget(self.central_widget)
+    
+    def set_menubar(self):
+        self.menubar = QMenuBar(self)
+        self.setMenuBar(self.menubar)
+        
+        self.file_menu = QMenu(self.menubar)
+        self.file_menu.setTitle('&파일')
+        self.menubar.addMenu(self.file_menu)
+        
+        self.set_file_action()
+        
+        self.edit_menu = QMenu(self.menubar)
+        self.edit_menu.setTitle('&편집')
+        self.menubar.addMenu(self.edit_menu)
+    
+    def set_file_action(self):
+        self.new_action = QAction('&새로 만들기')
+        self.new_action.setShortcut('Ctrl+N')
+        
+        self.new_window_action = QAction('&새 창')
+        self.new_window_action.setShortcut('Ctrl+Shift+N')
+        
+        self.open_action = QAction('&열기')
+        self.open_action.setShortcut('Ctrl+O')
+        
+        self.save_action = QAction('&저장')
+        self.save_action.setShortcut('Ctrl+S')
+        
+        self.saveas_action = QAction('&다른 이름으로 저장')
+        self.saveas_action.setShortcut('Ctrl+Shift+S')
+        
+        self.file_separator1 = self.file_menu.addSeparator()
+        
+        self.page_action = QAction('&페이지 설정')
+        
+        self.print_action = QAction('&인쇄')
+        self.print_action.setShortcut('Ctrl+P')
+        
+        self.file_separator2 = self.file_menu.addSeparator()
+        
+        self.exit_action = QAction('&끝내기')
+        
+        self._action = QAction('&')
+        self._action.setShortcut('Ctrl+Shift+')
+        
+        self.file_menu.addActions([
+            self.new_action,
+            self.new_window_action,
+            self.open_action,
+            self.save_action,
+            self.saveas_action,
+            self.file_separator1,
+            self.page_action,
+            self.print_action,
+            self.file_separator2,
+            self.exit_action
+            ])
+    
+        self.statusbar = QStatusBar(self)
+        self.setStatusBar(self.statusbar)
+
+app = QApplication(sys.argv)
+window = MainWindow()
+app.exec()
