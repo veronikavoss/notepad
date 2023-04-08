505ee5c0de927e5375c2d568bf5b1c7058eca6ef
veronikavoss <veronikavoss.none@gmail.com>
2023-04-08 13:13:45 +0900

    01. open action

--- actions.py
+++ actions.py
@@ -1,26 +1,43 @@
-from PySide6.QtWidgets import QFileDialog
+from PySide6.QtWidgets import QFileDialog,QMessageBox
 import os
 
 class SetActions:
     def new(self):
-        pass
+        self.plain_text_edit.clear()
     
     def new_window(self):
-        pass
+        self.save_messagebox()
     
     def open(self):
-        title = self.windowTitle()
-        self.file_name = QFileDialog.getOpenFileName(self)
-        print('file_name',os.path.join(os.path.basename(self.file_name[0])))
-        try:
-            self.setWindowTitle(os.path.join(os.path.basename(self.file_name[0])) + ' - Windows 메모장')
-            with open(self.file_name[0],'r',encoding='UTF8') as r:
-                text = r.read()
-            self.plain_text_edit.setPlainText(text)
-            self.original_text = self.plain_text_edit.toPlainText()
-            print(self.original_text)
-        except:
-            self.setWindowTitle(title)
+        self.previous_filename = self.windowTitle()
+        if not self.modify:
+            print(self.file_name)
+            self.file_name = QFileDialog.getOpenFileName(self,'열기','',self.filter_option)[0]
+            if self.file_name:
+                try:
+                    with open(self.file_name,'r',encoding='ansi') as r:
+                        text = r.read()
+                        self.original_text = text
+                        r.close()
+                except Exception as e:
+                    print(e)
+                    # self.setWindowTitle(self.previous_filename)
+                else:
+                    self.plain_text_edit.setPlainText(text)
+            else:
+                self.file_name = self.previous_filename
+        else:
+            self.save_messagebox()
+    
+    def save_messagebox(self):
+        messagebox=QMessageBox()
+        messagebox.setWindowTitle('메모장')
+        messagebox.setText('변경 내용을 {}에 저장 하시겠습니까?'.format(
+            os.path.basename(self.file_name) if self.file_name else '제목 없음'))
+        messagebox.addButton('저장(S)',QMessageBox.YesRole)
+        messagebox.addButton('저장 안 함(N)',QMessageBox.NoRole)
+        messagebox.addButton('취소',QMessageBox.RejectRole)
+        self.get_messagebox_button=messagebox.exec()
     
     def save(self):
         self.file_name = QFileDialog.getSaveFileName(self)
--- notepad.py
+++ notepad.py
@@ -11,15 +11,22 @@ image_path = os.path.dirname(os.path.abspath(__file__))
 
 class MainWindow(QMainWindow,SetActions):
     def __init__(self):
-        super().__init__()
+        QMainWindow.__init__(self)
+        self.set_init()
         self.set_ui()
         self.set_menu()
         self.show()
     
-    def set_ui(self):
+    def set_init(self):
         self.file_name = '제목 없음'
+        self.previous_filename = self.file_name
+        self.window_title = self.file_name + ' - Windows 메모장'
+        self.modify = False
+        self.filter_option='텍스트 문서 (*.txt);;모든 파일 (*.*)'
+    
+    def set_ui(self):
         self.setWindowIcon(QIcon('./image/notepad_icon.png'))
-        self.setWindowTitle(self.file_name + ' - Windows 메모장')
+        self.setWindowTitle(self.window_title)
         self.resize(1280,720)
         
         self.central_widget = QWidget(self)
@@ -32,7 +39,6 @@ class MainWindow(QMainWindow,SetActions):
         self.vlayout.addWidget(self.plain_text_edit)
         self.original_text = self.plain_text_edit.toPlainText()
         self.plain_text_edit.textChanged.connect(self.checking_modify_document)
-        
         self.statusbar = QStatusBar(self)
         self.setStatusBar(self.statusbar)
         
@@ -42,12 +48,19 @@ class MainWindow(QMainWindow,SetActions):
         # self.grid_layout.addLayout(self.vlayout,0,0,1,1)
     
     def checking_modify_document(self):
+        # if not self.file_name:
+        #     self.file_name = self.setWindowTitle(self.previous_filename)
+        # else:
+        #     self.file_name = os.path.basename(self.file_name)
+        
         if self.original_text != self.plain_text_edit.toPlainText():
             print('modify')
-            self.setWindowTitle(f'*{self.windowTitle()}')
+            self.setWindowTitle(f'*{os.path.basename(self.file_name)}' + ' - Windows 메모장')
+            self.modify = True
         elif self.original_text == self.plain_text_edit.toPlainText():
             print('no modify')
-            self.setWindowTitle(f'{self.windowTitle()}')
+            self.setWindowTitle(f'{os.path.basename(self.file_name)}' + ' - Windows 메모장')
+            self.modify = False
     
     def set_menu(self):
         self.menubar = QMenuBar(self)
@@ -67,9 +80,11 @@ class MainWindow(QMainWindow,SetActions):
         
         self.new_action = QAction('새로 만들기(&N)')
         self.new_action.setShortcut('Ctrl+N')
+        self.new_action.triggered.connect(self.new)
         
         self.new_window_action = QAction('새 창(&W)')
         self.new_window_action.setShortcut('Ctrl+Shift+N')
+        self.new_window_action.triggered.connect(self.new_window)
         
         self.open_action = QAction('열기(&O)...')
         self.open_action.setShortcut('Ctrl+O')
@@ -92,6 +107,7 @@ class MainWindow(QMainWindow,SetActions):
         self.file_separator2 = self.file_menu.addSeparator()
         
         self.exit_action = QAction('끝내기(&X)')
+        self.exit_action.triggered.connect(self.close)
         
         self._action = QAction('&')
         self._action.setShortcut('Ctrl+Shift+')
@@ -108,6 +124,11 @@ class MainWindow(QMainWindow,SetActions):
             self.file_separator2,
             self.exit_action
             ])
+    
+    def closeEvent(self,event):
+        print('close')
+        if self.modify:
+            event.ignore()
 
 app = QApplication(sys.argv)
 window = MainWindow()
